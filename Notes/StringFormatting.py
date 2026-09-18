d562e570ccd85710b45f0a06616f1146b24304e7
Sahan3141 <sahankumar3141@gmail.com>
2026-09-18 22:35:47 +0530

    edit to str formatting

--- Notes/StringFormatting.py
+++ Notes/StringFormatting.py
@@ -2,3 +2,30 @@
 name = "John"
 place = "New York"
 print("Hello, my name is {0}, I am from {1}".format(name,place))
+
+#Syntax for f-string formatting
+name = "John"   
+place = "New York"
+print(f"Hello, my name is {name}, I am from {place}")
+
+#Syntax for %-formatting
+name = "John"
+place = "New York"
+print("Hello, my name is %s, I am from %s" % (name, place))
+
+#Syntax for Template string formatting
+from string import Template
+name = "John"
+place = "New York"
+template = Template("Hello, my name is $name, I am from $place")
+print(template.substitute(name=name, place=place))
+
+#Syntax for concatenation
+name = "John"   
+place = "New York"
+print("Hello, my name is " + name + ", I am from " + place)
+
+#Syntax for join method
+name = "John"   
+place = "New York"
+print("Hello, my name is " + " ".join([name]) + ", I am from " + " ".join([place]))
