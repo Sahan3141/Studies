585f32a732c804bc732e273bac6174934df3d265
Sahan3141 <sahankumar3141@gmail.com>
2026-09-19 16:35:15 +0530

    Edit to stringformatting

--- Notes/StringFormatting.py
+++ Notes/StringFormatting.py
@@ -1,31 +1,27 @@
-#Syntax for string formatting
 name = "John"
 place = "New York"
-print("Hello, my name is {0}, I am from {1}".format(name,place))
 
-#Syntax for f-string formatting
-name = "John"   
-place = "New York"
+#Syntax for string formatting -- Use for dynamic strings
+print("Hello, my name is {}, I am from {}".format(name,place))
+
+#Syntax for f-string formatting -- Latest and most preferred way of string formatting
 print(f"Hello, my name is {name}, I am from {place}")
 
 #Syntax for %-formatting
-name = "John"
-place = "New York"
 print("Hello, my name is %s, I am from %s" % (name, place))
 
-#Syntax for Template string formatting
-from string import Template
-name = "John"
-place = "New York"
-template = Template("Hello, my name is $name, I am from $place")
-print(template.substitute(name=name, place=place))
+# #Syntax for Template string formatting -- Not that useful
+# from string import Template
+# template = Template("Hello, my name is $name, I am from $place")
+# print(template.substitute(name=name, place=place))
 
-#Syntax for concatenation
-name = "John"   
-place = "New York"
+#Syntax for concatenation -- Use when creating string via some algorithm
 print("Hello, my name is " + name + ", I am from " + place)
 
-#Syntax for join method
-name = "John"   
-place = "New York"
+#Syntax for join method -- Use when creating string via some algorithm
+
+a = ["Hello", "I", "am", "John"]
+print("-/\-".join(a))
+
 print("Hello, my name is " + " ".join([name]) + ", I am from " + " ".join([place]))
+
